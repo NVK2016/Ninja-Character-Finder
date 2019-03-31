0548976f0dd2d64a1e599d510325b186041e4b8b
Namita Shenai <namita.shenai@gmail.com>
2019-03-31 12:22:17 -0700

    ApI Get / POst request from survey html & api route js page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,29 @@
-var friendList = [
-    {name : 'Simsons', },
-    {name : 'Mr.snuggles', },
-    {name : 'Lady bug', }
-]; 
+var friendsArray = [
+    {
+        name: 'Leonardo',
+        photo: "",
+        scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
+    },
+    {
+        name: 'Raphael',
+        photo: "",
+        scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
+    },
+    {
+        name: 'Donatello', 
+        photo: "",
+        scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
+    },
+    {
+        name: 'Michelangelo', 
+        photo: "https://www.sideshow.com/collectibles/tmnt-michelangelo-prime-1-studio-902362", 
+        scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
+    },
+    {
+        name: 'Splinter', 
+        photo: "", 
+        scores: [1, 2, 3, 4, 5, 1, 5, 4, 3, 2]
+    }
+];
 
-module.exports = friendList; 
+module.exports = friendsArray; 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,16 @@
 var friendList = require('../data/friends');
 
 module.exports = function(app){
+  //API GET Requests 
   //a GET route that displays JSON of all possible friends
   app.get('/api/friends', function(req,res){
     res.json(friendList);
-  }) };
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  app.post('/api/friends', function(req, res){
+    friendList.push(req.body); 
+    res.json(); 
+  })
+};
