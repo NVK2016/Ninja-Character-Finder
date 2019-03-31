d0d6afa79abce072950d542f9c6c822bfdc81373
Namita Shenai <namita.shenai@gmail.com>
2019-03-31 12:59:46 -0700

    api Route friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,7 @@ var friendList = require('../data/friends');
 
 module.exports = function(app){
   //API GET Requests 
-  //a GET route that displays JSON of all possible friends
+  //returns all the friends from the file in a JSON format 
   app.get('/api/friends', function(req,res){
     res.json(friendList);
   });
@@ -11,7 +11,16 @@ module.exports = function(app){
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   app.post('/api/friends', function(req, res){
+
+    //Friend Details 
+    console.log(req.body.scores);
+    console.log(req.body.name);
+    console.log(req.body.photo);
+
+    //Adding the new friend to the list 
     friendList.push(req.body); 
+
+    //Sending the match back to the browser 
     res.json(); 
   })
 };
