329cc8761ba454e2678043fc9e39d6e1dead76db
Namita Shenai <namita.shenai@gmail.com>
2019-04-01 09:39:31 -0700

    connected to heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,7 +15,7 @@ module.exports = function(app){
     //Data related 
     var newFriend = req.body;
     var newFriendScores = []; 
-
+    console.log("POst request");
     //Convert the scores
     for(var i = 0; i < newFriend.scores.length; i++) {
       newFriendScores.push(parseInt(newFriend.scores[i]));
@@ -36,32 +36,39 @@ module.exports = function(app){
 
 function matchNinjaCharacter(newFriend){
   //Loop through all the TMNT Characters 
+  
+  console.log("TMNT" + friendList.length); 
+
+  var matchCharacter ; 
+  var totalDiff = 7; 
+
   for(var i =0; i < friendList.length; i++){
-    var totalDiff = 0, charcterScore = 0,  friendScore =0; 
+
     
-    for (let j = 0; j < friendList[i]["scores"].length; j++) {
+    
+    //Compute Difference between each question 
+    for (var j = 0; j < newFriend.scores.length; j++) {
+
+      //REset Values 
+      var questionDiff = 0, charcterScore = 0,  newFriendScore =0; 
+      
       charcterScore += parseInt(friendList[i]["scores"][j]);
       
+      newFriendScore += parseInt(newFriend.scores[j]); 
+      questionDiff += charcterScore - newFriendScore ; 
+      // console.log("TMNT total: "+ charcterScore); 
+      // console.log("Friend Score: "+ newFriendScore); 
+      console.log("TOtal Diff: "+ questionDiff); 
     }
-    
-    for(var idx = 0; i < newFriend.scores.length; idx++) {
-      friendScore += (parseInt(newFriend.scores[idx]));
-    }
-    console.log("TMNT total: "+ charcterScore); 
-    console.log("New Frien total: "+ friendScore); 
-
-    totalDiff = friendScore - charcterScore ; 
-    if (totalDiff < charcterScore ){ 
-      console.log("Prefect match" + friendList[i].name); 
-    }
-    else { 
-        //default 
-        friendList[0];
-    }
+    //Closest DIffernce 
+      if( questionDiff < totalDiff ){ 
+        totalDiff = questionDiff; 
+        matchCharacter =  friendList[i]; 
+      }
      
-      //REturn the friend 
-      return friendList[i]; 
-    // }
   }
+
+   //Return the Matched Character 
+   return matchCharacter; 
  
 }
