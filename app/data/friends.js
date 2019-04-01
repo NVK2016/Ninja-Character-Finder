d4587d70211732e6c4f1002bddf5c6ad46d860e1
Namita Shenai <namita.shenai@gmail.com>
2019-04-01 09:52:36 -0700

    Changed the image path on the data folder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,28 @@
 var friendsArray = [
     {   //Score <= 7 
         name: 'Leonardo',
-        photo: "http://localhost:3000/assets/images/Leonardo.gif",
+        photo: "/assets/images/Leonardo.gif",
         scores: [1,1,1,1,1,1,1],
         desc: "Weapon: Katanas \n Favourite Colour: Blue \n Hobbies: Meditating\nNicknames: Leo, Leon, 'Fearless', Leader, Ginsu Master\n Favourite Food: Rice, fish, salads, apples, pizza"
     },
     {   //Score > 7 & <=14 
         name: 'Raphael',
-        photo: "http://localhost:3000/assets/images/Raphael.gif",
+        photo: "/assets/images/Raphael.gif",
         scores: [2,2,2,2,2,2,2]
     },
     {   //Score > 14 & <= 21
         name: 'Donatello', 
-        photo: "http://localhost:3000/assets/images/donatello.gif",
+        photo: "/assets/images/donatello.gif",
         scores: [3,3,3,3,3,3,3]
     },
     {   //Score > 21 & <= 28 
         name: 'Michelangelo', 
-        photo: "http://localhost:3000/assets/images/michealangelo.gif",
+        photo: "/assets/images/michealangelo.gif",
         scores: [4,4,4,4,4,4,4]
     },
     {   //Score >28 
         name: 'Master Splinter', 
-        photo: "http://localhost:3000/assets/images/masterSplinter.gif",
+        photo: "/assets/images/masterSplinter.gif",
         scores: [5,5,5,5,5,5,5]
     }
 ];
--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 //the path that you provide to the express.static function is relative to the directory from where you launch your node process. 
 //If you run the express app from another directory, it’s safer to use the absolute path of the directory that you want to serve:
 
-app.use('/static', express.static(path.join(__dirname, '/app/public')));
+// app.use('/static', express.static(path.join(__dirname, '/app/public')));
+app.use(express.static('/app/public'));
 
 // ROUTER
 // =============================================================
