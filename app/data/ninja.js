8d8c103d7a3f964b4fdcfd1e8a4bc2fc0bc36006
Namita Shenai <namita.shenai@gmail.com>
2019-04-01 20:03:25 -0700

    Add desc for characters

--- app/data/ninja.js
+++ app/data/ninja.js
@@ -3,27 +3,31 @@ var ninjaArray = [
         name: 'Leonardo',
         photo: "/assets/images/Leonardo.gif",
         scores: [1,1,1,1,1,1,1],
-        desc: "Weapon: Katanas \n Favourite Colour: Blue \n Hobbies: Meditating\nNicknames: Leo, Leon, 'Fearless', Leader, Ginsu Master\n Favourite Food: Rice, fish, salads, apples, pizza"
+        desc: "Weapon: Katanas \n Favourite Colour: Blue \n Hobbies: Meditating\nNicknames: Leo, Leon, 'Fearless', Leader, Ginsu Master\n Favourite Food: Rice, fish, salads, apples, pizza \n Leonardo is the leader of the turtles. He takes everything very seriously especially his Ninjitsu training. Since Leo doesn't have many hobbies, Leo likes to read. But he doesn't read just any kind of books. He reads books about strategy for the protection of his family. Some say Leonardo's expectations of things are too high but Leo is just trying to make things done well. Leonardo is always prepared and protects his family before he himself in all situations."
     },
     {   //Score > 7 & <=14 
         name: 'Raphael',
         photo: "/assets/images/Raphael.gif",
-        scores: [2,2,2,2,2,2,2]
+        scores: [2,2,2,2,2,2,2],
+        desc: "Weapon: Sai \n Favourite Colour: Red \n Hobbies: Hockey, baseball, golf, fighting \n Raphael is the most hotheaded turtle of all. He's too rational which usually causes more trouble for the turtles but sometimes, Raph's character is helpful in certain situations. Raphael is anti-social and short-tempered. When people get on his nerves (a lot of people do), Raph likes to be alone. At the moment, Raph is trying to control himself and as Splinter told him to find his 'center'."
     },
     {   //Score > 14 & <= 21
         name: 'Donatello', 
         photo: "/assets/images/donatello.gif",
-        scores: [3,3,3,3,3,3,3]
+        scores: [3,3,3,3,3,3,3], 
+        desc: "Weapon: Bo \n Favourite Colour: Purple\n Hobbies: Inventing, Computers, Engineering\n Favourite Food: Pizza, Sushi \n Donatello is the 'brains' of the Turtles. He's studious and industrious. He fixes anything and everything. Don isn't like the others when it comes to fighting. Donny would rather talk things out instead of physical contact. But most of the time, talking won't solve anything. Donny is always busy with his inventions and is working on a violent-free world invention by changing the genes. Like it says in the 1980's TMNT cartoon series introduction song, 'Donatello does machines!' "
     },
     {   //Score > 21 & <= 28 
         name: 'Michelangelo', 
         photo: "/assets/images/michelangelo.gif",
-        scores: [4,4,4,4,4,4,4]
+        scores: [4,4,4,4,4,4,4], 
+        desc: "Weapon: Nunchaku\n Favourite Colour: Orange\n Hobbies: Skating, Surfing, video games\n Nicknames: Mickey \n Favourite Food: Pizza \n Mickey is the 'party dude'. He's funny and very goofy but he's fun. When things are down, Mickey's there to cheer up the rest of the turtles. Mickey LOVES pizza! No one loves pizza more than Mickey! Michlangelo has the cool nunchaku weapon. Not only can he use it to fight the bad guys; he can use them to fly in the air by spinning them really fast like a helicopter. Mickey is always in a good mood. It probably because he keeps saying, 'Man, I love being a turtle'."
     },
     {   //Score >28 
         name: 'Master Splinter', 
         photo: "/assets/images/masterSplinter.gif",
-        scores: [5,5,5,5,5,5,5]
+        scores: [5,5,5,5,5,5,5], 
+        desc: "Weapon: Any\n Favourite Colour: Yellow \n  Hobbies: Reading, cooking\n Nicknames: None\n Favourite Food: vegetables, soup, tea\n Splinter or Master Splinter is the sensei of the turtles. (Sensei means teacher) Not only does he teach the turtles the art of Ninjitsu, he is also their father. As the turtles grew up, Splinter was the only creature they knew. There are two different stories of how Splinter became what he is. One story is that he used to be a man named Hamato Yoshi who turned into a rat. The original story from the original comic books and the new t.v. series tells the story of Splinter being a small pet rat belonging to a man named Hamato Yoshi. As a pet rat, he copied all of Yoshi's moves and became a master at ninjitsu. When his master Yoshi was killed, Splinter escaped from the house and came to the sewers where he met the turtles. Splinter and the turtles were covered in ooze that made all of them bigger."
     }
 ];
 
